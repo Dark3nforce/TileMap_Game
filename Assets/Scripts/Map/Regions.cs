7841dbd1fab27cfa8616221a3c45400745d50f4e
Dark3nforce <jzoretic95@gmail.com>
2019-08-19 11:33:12 -0400

    initial upload

---
+++ Assets/Scripts/Map/Regions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Regions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public enum BiomeList
+{
+    Grass,
+    Sand,
+    Water,
+    Ice
+}
---
+++ Assets/Scripts/Pokemon/BasePokemon.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BasePokemon : MonoBehaviour {
+
+    public string PName;
+    public Sprite image;
+    public BiomeList biomeFound;
+    public PokemonType type;
+    public Rarity rarity;
+    public int HP;
+    private int maxHP;
+    public Stat AttackStat;
+    public Stat DefenceStat;
+
+    public PokemonStats pokemonStats;
+
+    public bool canEvolve;
+    public PokemonEvolution evolveTo;
+
+    private int level;
+
+	void Start () {
+        maxHP = HP;
+	}
+	void Update () {
+	
+	}
+
+    public void AddMember(BasePokemon bp)
+    {
+        this.PName = bp.PName;
+        this.image = bp.image;
+        this.biomeFound = bp.biomeFound;
+        this.type = bp.type;
+        this.rarity = bp.rarity;
+        this.HP = bp.HP;
+        this.maxHP = bp.maxHP;
+        this.AttackStat = bp.AttackStat;
+        this.DefenceStat = bp.DefenceStat;
+        this.pokemonStats = bp.pokemonStats;
+        this.canEvolve = bp.canEvolve;
+        this.evolveTo = bp.evolveTo;
+        this.level = bp.level;
+    }
+}
+
+public enum Rarity
+{
+    VeryCommon,
+    Common,
+    SemiRare,
+    Rare,
+    VeryRare
+}
+
+public enum PokemonType
+{
+    Flying,
+    Ground,
+    Rock,
+    Steel,
+    Fire,
+    Water,
+    Grass,
+    Ice,
+    Electric,
+    Psychic,
+    Dark,
+    Dragon,
+    Fighting,
+    Normal
+}
+
+[System.Serializable]
+public class PokemonEvolution
+{
+    public BasePokemon nextEvolution;
+    public int levelUpLevel;
+}
+
+[System.Serializable]
+public class PokemonStats
+{
+    public int AttackStat;
+    public int DefenceStat;
+    public int SpeedStat;
+    public int SpAttackStat;
+    public int SpDefenceStat;
+    public int EvasionStat;
+}
