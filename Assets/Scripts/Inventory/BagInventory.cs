ef2290cbec032e9d6ec2628be9a1f9f4fdcf350f
scassagnol <46454791+scassagnol@users.noreply.github.com>
2019-09-13 23:06:32 -0400

    inventory
    able to collect different items

--- Assets/Scripts/Inventory/BagInventory.cs
+++ Assets/Scripts/Inventory/BagInventory.cs
@@ -10,6 +10,26 @@ public class BagInventory : MonoBehaviour
         
     }
 
+    public static int potion;
+    public static int pokeball;
+
+
+    void OnTriggerEnter2D(Collider2D collide)
+    {
+
+        if (collide.gameObject.tag == "Player")
+        { if (gameObject.tag == "Potion") {
+                Destroy(gameObject);
+                potion++;
+                print("You now have " + potion + " * potion");
+            }
+            else if (gameObject.tag == "pokeball") {
+                Destroy(gameObject);
+                pokeball++;
+                print("You now have " + pokeball +" * pokeball");
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
