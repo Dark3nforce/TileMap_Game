ce995c8810ec41c92ce59b7d10d2b4cadcdab665
scassagnol <46454791+scassagnol@users.noreply.github.com>
2019-09-15 20:33:34 -0400

    Update BagInventory.cs

--- Assets/Scripts/Inventory/BagInventory.cs
+++ Assets/Scripts/Inventory/BagInventory.cs
@@ -7,12 +7,15 @@ public class BagInventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        inventory = new int [pokeball];
+        inventory = new int[potion];
+
     }
 
+    public int[] inventory;
     public static int potion;
     public static int pokeball;
-
+    
 
     void OnTriggerEnter2D(Collider2D collide)
     {
@@ -22,13 +25,16 @@ public class BagInventory : MonoBehaviour
                 Destroy(gameObject);
                 potion++;
                 print("You now have " + potion + " * potion");
+                print(inventory);
             }
             else if (gameObject.tag == "pokeball") {
                 Destroy(gameObject);
                 pokeball++;
                 print("You now have " + pokeball +" * pokeball");
+                print(inventory);
             }
         }
+       
     }
     // Update is called once per frame
     void Update()
