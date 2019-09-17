fa63ac4ced54a8d18bb7d115ec0cb1d52ec4aa1c
Joseph Zoretic <jzoretic95@gmail.com>
2019-09-17 11:41:43 -0400

    battle triggers cont.

--- Assets/Scripts/Map/LongGrass.cs
+++ Assets/Scripts/Map/LongGrass.cs
@@ -12,6 +12,7 @@ public class LongGrass : MonoBehaviour {
     private float sr = 6.75f / 187.5f;
     private float r = 3.33f / 187.5f;
     private float vr = 1.25f / 187.5f;
+    // private float p;
     public Rarity raritySet;
     public bool triggered = false;
 
@@ -26,11 +27,11 @@ public class LongGrass : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("entering: " + triggered + " gm: " + gm);
+        // Debug.Log("entering: " + triggered + " gm: " + gm);
         // if(col.tag == "Long_Grass") {
-            Debug.Log("entering: " + triggered + " gm: " + gm);
-            // if(col.GetComponent<PlayerMovement>())
-            // {
+        //     Debug.Log("entering: " + triggered + " gm: " + gm);
+        //     if(col.GetComponent<PlayerMovement>())
+        //     {
                 //P = x / 187.5
                 //VC = 10, C = 8.5, Semi-Are = 6.75, Rare = 3.33, VR = 1.25
                 // float vc = 10 / 187.5f;
@@ -85,7 +86,7 @@ public class LongGrass : MonoBehaviour {
                         }
                     }   
                 }
-            // }
+        //     }
         // }
     }
 
