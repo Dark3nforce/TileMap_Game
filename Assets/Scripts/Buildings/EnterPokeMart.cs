9d46cdc7ab7e136d937484faa063b29030ff2125
scassagnol <46454791+scassagnol@users.noreply.github.com>
2019-11-11 11:27:51 -0500

    connected more scenes

---
+++ Assets/Scripts/Buildings/EnterPokeMart.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterPokeMart : MonoBehaviour
+{
+    public GameObject theManager;
+    public GameObject player;
+    public bool active;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scene home = SceneManager.GetSceneByName("");
+
+        if (active && Input.GetButtonDown("Fire1"))
+        {
+            load("PokeMart_interior");
+            // DontDestroyOnLoad(theManager);
+
+            player.transform.position = new Vector3(5319, -3830, 0);
+
+        }
+
+    }
+
+    public void load(string Location)
+    {
+        SceneManager.LoadSceneAsync("PokeMart_interior", LoadSceneMode.Additive);
+        //SceneManager.LoadSceneAsync("pokecenter_interior");
+        // SceneManager.UnloadSceneAsync("OverWorld");
+
+
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            active = true;
+            Debug.Log("active");
+
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            active = false;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Buildings/ExitPokeMart.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitPokeMart : MonoBehaviour
+{
+    GameObject player;
+    public bool active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //DontDestroyOnLoad(theManager);
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (active && Input.GetButtonDown("Fire1"))
+        {
+
+            load("OverWorld");
+
+            player.transform.position = new Vector3(-68, 13, 0);
+            //DontDestroyOnLoad(player);
+        }
+
+    }
+
+    public void load(string PlayerHome)
+    {
+
+        //SceneManager.LoadSceneAsync("OverWorld");
+        SceneManager.UnloadSceneAsync("PokeMart_interior");
+
+
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            active = true;
+            Debug.Log("active");
+
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            active = false;
+        }
+    }
+}
